9e0130f9b8c8ff25328f95e8ea27059b08c155ec
Felix <felipinhdez03@gmail.com>
2023-11-07 06:07:47 -0600

    post CRUD

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Red
 
 Las vistas estan en la carpeta ./src/views/
-Cada subcarpeta es una vista, con index.hbs siendo usualmente la vista
+Cada subcarpeta es una vista, con index.hbs siendo usualmente el menu principal de la vista
+
+Para correr utilizar los siguientes comandos:
+```
+npm install
+npm run dev
+```
--- src/routes/link.js
+++ src/routes/link.js
@@ -2,8 +2,60 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 
-router.get('/', (req, res) => {
-  res.render('posts');
+router.get('/', async (req, res) => {
+  const posts = await pool.query('SELECT * FROM posts');
+  res.render('posts', { posts });
+});
+
+router.get('/add', (req, res) => {
+  addPostData = {
+    formTitle: "Agregar un post nuevo",
+    formDestination: "/posts/add",
+    titleData: "",
+    descriptionData: "",
+  }
+  res.render('posts/addPost', addPostData);
+});
+
+router.post('/add', async (req, res) => {
+  const { title, description } = req.body;
+  //TODO: add user_id support
+  const newPost = {
+      title,
+      description,
+  };
+  await pool.query('INSERT INTO posts set ?', [newPost]);
+  res.redirect('/posts');
+});
+
+router.get('/delete/:id', async (req, res) => {
+  const { id } = req.params;
+  await pool.query('DELETE FROM posts WHERE id = ?', [id]);
+  res.redirect('/posts');
+});
+
+router.get('/edit/:id', async (req, res) => {
+  const { id } = req.params;
+  const posts = await pool.query('SELECT * FROM posts WHERE id = ?', [id]);
+  const post = posts[0]
+  editPostData = {
+    formTitle: "Editar post",
+    formDestination: "/posts/edit/" + id,
+    titleData: post.title,
+    descriptionData: post.description,
+  }
+  res.render('posts/addPost', editPostData);
+});
+
+router.post('/edit/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, description} = req.body; 
+  const newPost = {
+      title,
+      description
+  };
+  await pool.query('UPDATE posts set ? WHERE id = ?', [newPost, id]);
+  res.redirect('/posts');
 });
 
 module.exports = router;
