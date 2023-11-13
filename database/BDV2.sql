1da2ea200767fcffd08743ab1d3366c3cbc64224
Felix <felipinhdez03@gmail.com>
2023-11-13 14:04:10 -0600

    upvotes

--- database/BDV2.sql
+++ database/BDV2.sql
@@ -1,97 +1,180 @@
-    DROP DATABASE BDV2;
-    CREATE DATABASE IF NOT EXISTS BDV2;
-    USE BDV2;
-
-    CREATE TABLE UserTables(
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        description TINYTEXT NOT NULL
-    );
-
-    CREATE TABLE Users (
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        control_number VARCHAR(30) NOT NULL UNIQUE,
-        password VARCHAR(60) NOT NULL,
-        full_name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL,
-        img varchar(255) NOT NULL DEFAULT 'default.jpg',
-        privilege INT NOT NULL DEFAULT 0,
-        status INT NOT NULL DEFAULT 0 REFERENCES UserTables(id)
-    );
-
-    CREATE TABLE Posts (
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        content TEXT NOT NULL,
-        userFK INT REFERENCES Users(id) ON DELETE CASCADE
-    );
-
-    CREATE TABLE Messages(
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        content TEXT,
-        user_senderFK INT REFERENCES Users(id) ON DELETE CASCADE,
-        user_receiverFK INT REFERENCES Users(id) ON DELETE CASCADE
-    );
-
-    CREATE TABLE Follows(
-        followedFK INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
-        followerFK INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
-        PRIMARY KEY (followedFK, followerFK)
-    );
-
-    -- sessions table for express-sessions internals
-    CREATE TABLE sessions (
+DROP DATABASE BDV2;
+CREATE DATABASE IF NOT EXISTS BDV2;
+USE BDV2;
+CREATE TABLE UserTables(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    description TINYTEXT NOT NULL
+);
+CREATE TABLE Users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    control_number VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    img varchar(255) NOT NULL DEFAULT 'default.jpg',
+    privilege INT NOT NULL DEFAULT 0,
+    status INT NOT NULL DEFAULT 0 REFERENCES UserTables(id)
+);
+CREATE TABLE Posts (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content TEXT NOT NULL,
+    userFK INT REFERENCES Users(id) ON DELETE CASCADE
+);
+CREATE TABLE Messages(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content TEXT,
+    user_senderFK INT REFERENCES Users(id) ON DELETE CASCADE,
+    user_receiverFK INT REFERENCES Users(id) ON DELETE CASCADE
+);
+CREATE TABLE Follows(
+    followedFK INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
+    followerFK INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
+    PRIMARY KEY (followedFK, followerFK)
+);
+CREATE TABLE Upvotes(
+    userFK INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
+    postFK INT NOT NULL REFERENCES Posts(id) ON DELETE CASCADE,
+    value INT NOT NULL CHECK (value IN (1, -1)) DEFAULT 1,
+    PRIMARY KEY (userFK, postFK)
+);
+-- sessions table for express-sessions internals
+CREATE TABLE sessions (
     session_id varchar(128) NOT NULL PRIMARY KEY,
     expires int unsigned NOT NULL,
     data mediumtext,
     INDEX (expires)
+);
+CREATE USER 'tc_server' @'localhost' IDENTIFIED BY 'contrasemnia';
+GRANT ALL PRIVILEGES ON BDV2.* TO 'tc_server' @'localhost';
+FLUSH PRIVILEGES;
+INSERT INTO UserTables (description)
+VALUES ('usuario normal'),
+    ('egresado');
+-- PASSWORDS
+-- Administrator
+-- :D?B97SUE"82?xT
+-- ChavaPass
+-- FulanitoPass
+-- JosePass
+INSERT INTO Users (
+        control_number,
+        password,
+        full_name,
+        email,
+        img,
+        privilege,
+        status
+    )
+VALUES (
+        '0',
+        '$2a$10$6d2jeJ55rc1OqDpnX9ctKeV34qNfn9Ji78DO.qf4as4yCfcvbjgba',
+        'Administrador',
+        'Admin@google.com',
+        'default.jpg',
+        1,
+        0
+    ),
+    (
+        '210110037',
+        '$2a$10$N8PXR/ladUnljsUTbNHV6edDQNl5V5xnusgmZUNvp128M3DF/rjfm',
+        'Felipe',
+        'Felipinhdez@gmail.com',
+        'a1.jpg',
+        0,
+        0
+    ),
+    (
+        '210110038',
+        '$2a$10$Z9Y.OxcE.E4afRJA3r5bY.I11wqlG/8st0UFKsFMkuivlIVzMzlkO',
+        'Chava',
+        'chava1898@gmail.com',
+        'a2.jpg',
+        0,
+        0
+    ),
+    (
+        '210110047',
+        '$2a$10$OaKOJiCrk32tNuUwJnQotuMRFUpYJeHgcMaGC3Si.82jTDhBKT/ye',
+        'Ramiro',
+        'funalitodetal@gmail.com',
+        'a3.jpg',
+        0,
+        1
+    ),
+    (
+        '210110045',
+        '$2a$10$ce48PJwh2NqOGr8hI7CexOI0Yrg4UUo3xALiequdF7B7PESmaRymy',
+        'Jose Juan',
+        'Josejuan@gmail.com',
+        'a4.jpg',
+        0,
+        0
     );
-
-    CREATE USER 'tc_server'@'localhost' IDENTIFIED BY 'contrasemnia';
-    GRANT ALL PRIVILEGES ON BDV2.* TO 'tc_server'@'localhost';
-    FLUSH PRIVILEGES;
-
-    INSERT INTO UserTables (description)
-    VALUES 
-        ('usuario normal'),
-        ('egresado');
-
-    -- PASSWORDS
-    -- Administrator
-    -- :D?B97SUE"82?xT
-    -- ChavaPass
-    -- FulanitoPass
-    -- JosePass
-    INSERT INTO Users (control_number, password, full_name, email, img, privilege, status)
-    VALUES 
-        ('0', '$2a$10$6d2jeJ55rc1OqDpnX9ctKeV34qNfn9Ji78DO.qf4as4yCfcvbjgba', 'Administrador', 'Admin@google.com', 'default.jpg', 1, 0),
-        ('210110037', '$2a$10$N8PXR/ladUnljsUTbNHV6edDQNl5V5xnusgmZUNvp128M3DF/rjfm', 'Felipe', 'Felipinhdez@gmail.com', 'a1.jpg', 0, 0),
-        ('210110038', '$2a$10$Z9Y.OxcE.E4afRJA3r5bY.I11wqlG/8st0UFKsFMkuivlIVzMzlkO', 'Chava', 'chava1898@gmail.com', 'a2.jpg', 0, 0),
-        ('210110047', '$2a$10$OaKOJiCrk32tNuUwJnQotuMRFUpYJeHgcMaGC3Si.82jTDhBKT/ye', 'Fulanito', 'funalitodetal@gmail.com', 'a3.jpg', 0, 1),
-        ('210110045', '$2a$10$ce48PJwh2NqOGr8hI7CexOI0Yrg4UUo3xALiequdF7B7PESmaRymy', 'JoseJuan', 'Josejuan@gmail.com', 'a4.jpg', 0, 0);
-
-    INSERT INTO Posts (content, userFK)
-    VALUES 
-        ('Buenos días, ya están los resultados en SUBES para la beca jóvenes escribiendo el futuro para que revisen su estatus en el apartado de 
-Solicitud → solicitar beca →estatus', 1),
-        ('Buen dia el día viernes, de hoy en ocho sus clases las van a tomar en un laboratorio. La siguiente semana les confirmo en cual será solo por ese día. Ya que ocupamos de arreglar toda el aula de usos multiples para la semana de ciencia y tecnologia.', 2),
-        ('Perdón va ser a las 10 de la mañana.', 3),
-        ('Buen día recordarles que la evaluación de sus proyectos será el día martes 7 de noviembre a las 11:00 en su aula.', 3),
-        ('AVISO: A todos los que participaron en altares de muerto o alguna comisión en el evento y que no han entregado la relación de materia y unidad en que se les hará efectivo los 10 puntos extra, favor de entregar en el departamento de vinculación, a más tardar el próximo viernes.', 4),
-        ('REDi Inspira Arandas te invita a nuestra próxima conferencia "¿Quién quiere ser millonario? " Explora tecnologías  innovadoras ( NFTs, Blockchain y cripto) Impartida por Guillermo Gutiérrez.   No pierdas las oportunidad ', 5);
-
-    INSERT INTO Messages (content, user_senderFK, user_receiverFK)
-    VALUES 
-        ('Buenos días, ya están los resultados en SUBES para la beca jóvenes escribiendo el futuro para que revisen su estatus en el apartado de 
-Solicitud → solicitar beca →estatus', 1, 2),
-        ('Buen dia el día viernes, de hoy en ocho sus clases las van a tomar en un laboratorio. La siguiente semana les confirmo en cual será solo por ese día. Ya que ocupamos de arreglar toda el aula de usos multiples para la semana de ciencia y tecnologia.', 2, 3),
-        ('Buen día recordarles que la evaluación de sus proyectos será el día martes 7 de noviembre a las 11:00 en su aula.', 3, 4),
-        ('Perdón va ser a las 10 de la mañana.', 3, 4),
-        ('REDi Inspira Arandas te invita a nuestra próxima conferencia "¿Quién quiere ser millonario? " Explora tecnologías  innovadoras ( NFTs, Blockchain y cripto) Impartida por Guillermo Gutiérrez.   No pierdas las oportunidad ', 4, 5),
-        ('AVISO: A todos los que participaron en altares de muerto o alguna comisión en el evento y que no han entregado la relación de materia y unidad en que se les hará efectivo los 10 puntos extra, favor de entregar en el departamento de vinculación, a más tardar el próximo viernes.', 5, 1);
-
-    INSERT INTO Follows (followedFK, followerFK)
-    VALUES 
-        (1, 2),
-        (2, 3),
-        (3, 4),
-        (4, 5),
-        (5, 1);
+INSERT INTO Posts (content, userFK)
+VALUES (
+        'Buenos días, ya están los resultados en SUBES para la beca jóvenes escribiendo el futuro para que revisen su estatus en el apartado de 
+Solicitud → solicitar beca →estatus',
+        1
+    ),
+    (
+        'Buen dia el día viernes, de hoy en ocho sus clases las van a tomar en un laboratorio. La siguiente semana les confirmo en cual será solo por ese día. Ya que ocupamos de arreglar toda el aula de usos multiples para la semana de ciencia y tecnologia.',
+        2
+    ),
+    ('Perdón va ser a las 10 de la mañana.', 3),
+    (
+        'Buen día recordarles que la evaluación de sus proyectos será el día martes 7 de noviembre a las 11:00 en su aula.',
+        3
+    ),
+    (
+        'AVISO: A todos los que participaron en altares de muerto o alguna comisión en el evento y que no han entregado la relación de materia y unidad en que se les hará efectivo los 10 puntos extra, favor de entregar en el departamento de vinculación, a más tardar el próximo viernes.',
+        4
+    ),
+    (
+        'REDi Inspira Arandas te invita a nuestra próxima conferencia "¿Quién quiere ser millonario? " Explora tecnologías  innovadoras ( NFTs, Blockchain y cripto) Impartida por Guillermo Gutiérrez.   No pierdas las oportunidad ',
+        5
+    );
+INSERT INTO Messages (content, user_senderFK, user_receiverFK)
+VALUES (
+        'Buenos días, ya están los resultados en SUBES para la beca jóvenes escribiendo el futuro para que revisen su estatus en el apartado de 
+Solicitud → solicitar beca →estatus',
+        1,
+        2
+    ),
+    (
+        'Buen dia el día viernes, de hoy en ocho sus clases las van a tomar en un laboratorio. La siguiente semana les confirmo en cual será solo por ese día. Ya que ocupamos de arreglar toda el aula de usos multiples para la semana de ciencia y tecnologia.',
+        2,
+        3
+    ),
+    (
+        'Buen día recordarles que la evaluación de sus proyectos será el día martes 7 de noviembre a las 11:00 en su aula.',
+        3,
+        4
+    ),
+    ('Perdón va ser a las 10 de la mañana.', 3, 4),
+    (
+        'REDi Inspira Arandas te invita a nuestra próxima conferencia "¿Quién quiere ser millonario? " Explora tecnologías  innovadoras ( NFTs, Blockchain y cripto) Impartida por Guillermo Gutiérrez.   No pierdas las oportunidad ',
+        4,
+        5
+    ),
+    (
+        'AVISO: A todos los que participaron en altares de muerto o alguna comisión en el evento y que no han entregado la relación de materia y unidad en que se les hará efectivo los 10 puntos extra, favor de entregar en el departamento de vinculación, a más tardar el próximo viernes.',
+        5,
+        1
+    );
+INSERT INTO Follows (followedFK, followerFK)
+VALUES (1, 2),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 1);
+INSERT INTO Upvotes (userFK, postFK, value)
+VALUES (1, 1, 1),
+    (2, 2, 1),
+    (3, 3, 1),
+    (4, 4, 1),
+    (5, 5, 1),
+    (1, 2, -1),
+    (2, 3, 1),
+    (3, 4, -1),
+    (4, 5, 1),
+    (5, 1, -1);
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,20 +4,29 @@ const pool = require('../db');
 const helpers = require('../lib/helpers');
 
 router.get('/', helpers.isLoggedIn, async (req, res) => {
-  const posts = await pool.query(`SELECT
-  P.content AS content,
-  P.id AS id,
-  P.userFK AS userFK,
-  U.full_name AS user_name,
-  U.privilege AS user_privilege,
-  U.img AS user_pfp
-FROM
-  Posts P
-JOIN
-  Users U ON P.userFK = U.id
-ORDER BY P.id DESC`);
+  const posts = await pool.query(`
+    SELECT
+      P.content AS content,
+      P.id AS id,
+      P.userFK AS userFK,
+      U.full_name AS user_name,
+      U.privilege AS user_privilege,
+      U.img AS user_pfp,
+      COALESCE(SUM(UP.value), 0) AS votes
+    FROM
+      Posts P
+    JOIN
+      Users U ON P.userFK = U.id
+    LEFT JOIN
+      Upvotes UP ON P.id = UP.postFK
+    GROUP BY
+      P.id, P.content, P.userFK, U.full_name, U.privilege, U.img
+    ORDER BY
+      P.id DESC
+  `);
   res.render('posts', { posts });
 });
+
 
 router.get('/add', helpers.isLoggedIn, (req, res) => {
   addPostData = {
@@ -31,7 +40,6 @@ router.get('/add', helpers.isLoggedIn, (req, res) => {
 
 router.post('/add', helpers.isLoggedIn, async (req, res) => {
   const { content } = req.body;
-  //TODO: add user_id support
   const newPost = {
       content,
       userFK: req.user.id
@@ -80,5 +88,36 @@ router.post('/edit/:id', helpers.isLoggedIn, async (req, res) => {
   await pool.query('UPDATE posts set ? WHERE id = ?', [newPost, id]);
   res.redirect('/posts');
 });
+
+router.post('/upvote/:id', helpers.isLoggedIn, async (req, res) => {
+  const postId = req.params.id;
+  const userId = req.user.id;
+  const existingUpvote = await pool.query('SELECT * FROM Upvotes WHERE userFK = ? AND postFK = ?', [userId, postId]);
+  if (existingUpvote.length === 0) {
+    await pool.query('INSERT INTO Upvotes (userFK, postFK, value) VALUES (?, ?, 1)', [userId, postId]);
+  } else if (existingUpvote.length === 1 && existingUpvote[0].value === -1) {
+    await pool.query('UPDATE Upvotes SET value = 1 WHERE userFK = ? AND postFK = ?', [userId, postId]);
+  } else if (existingUpvote.length === 1) {
+    await pool.query('DELETE FROM Upvotes WHERE userFK = ? AND postFK = ?', [userId, postId]);
+  }
+
+  res.redirect('/posts');
+});
+
+router.post('/downvote/:id', helpers.isLoggedIn, async (req, res) => {
+  const postId = req.params.id;
+  const userId = req.user.id;
+  const existingDownvote = await pool.query('SELECT * FROM Upvotes WHERE userFK = ? AND postFK = ?', [userId, postId]);
+  if (existingDownvote.length === 0) {
+    await pool.query('INSERT INTO Upvotes (userFK, postFK, value) VALUES (?, ?, -1)', [userId, postId]);
+  } else if (existingDownvote.length === 1 && existingDownvote[0].value === 1) {
+    await pool.query('UPDATE Upvotes SET value = -1 WHERE userFK = ? AND postFK = ?', [userId, postId]);
+  } else if (existingDownvote.length === 1) {
+    await pool.query('DELETE FROM Upvotes WHERE userFK = ? AND postFK = ?', [userId, postId]);
+  }
+
+  res.redirect('/posts');
+});
+
 
 module.exports = router;
